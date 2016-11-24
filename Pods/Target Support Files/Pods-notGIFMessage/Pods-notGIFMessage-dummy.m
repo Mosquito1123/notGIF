576cd6db0b16263632e26501f1e5ce28d5ac8736
Atuooo <aaatuooo@gmail.com>
2016-11-25 00:47:53 +0800

    add .gitignore

--- Pods/Target Support Files/Pods-notGIFMessage/Pods-notGIFMessage-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_notGIFMessage : NSObject
-@end
-@implementation PodsDummy_Pods_notGIFMessage
-@end
--- Pods/Target Support Files/Pods-notGIFMessage/Pods-notGIFMessage-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_notGIFMessageVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_notGIFMessageVersionString[];
-
